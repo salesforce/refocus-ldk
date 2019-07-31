9a8f032ddb30a93c87f4f1c1733d6e5de834b5f4
Nazli Gungor <nazli.gungor@nazligunger-ltm.internal.salesforce.com>
2019-07-31 16:26:19 -0700

    util func and realtime test changes

--- src/lensinitUtils.js
+++ src/lensinitUtils.js
@@ -123,10 +123,10 @@ module.exports = {
       fs.readFileSync(path.resolve(__dirname, '../template/loading.handlebars'), 'utf8'));
     fs.appendFileSync(path.resolve(dir, 'test/SampleUtilTest.js'),
       fs.readFileSync(path.resolve(__dirname, '../template/testutil.template'), 'utf8'));
-    fs.appendFileSync(path.resolve(dir, 'test/SampleTest.js'),
+    fs.appendFileSync(path.resolve(dir, 'test/SampleRealtimeTest.js'),
       fs.readFileSync(path.resolve(__dirname, '../template/testsample.template'), 'utf8'));
-    fs.appendFileSync(path.resolve(dir, 'test/sample-json.json'),
-      fs.readFileSync(path.resolve(__dirname, '../template/sample-json.json'), 'utf8'));
+    fs.appendFileSync(path.resolve(dir, 'test/weatherByCountry.json'),
+      fs.readFileSync(path.resolve(__dirname, '../public/datasets/weatherByCountry.json'), 'utf8'));
     return true;
   },
 
